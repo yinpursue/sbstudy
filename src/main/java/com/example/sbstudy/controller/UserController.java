e5c154d18c104b26d707e0ed7b4e549480fb01ab
yinzhenzhao <yinpursue@163.com>
2018-11-02 11:37:47 +0800

    Spring Boot构建RESTful API与单元测试

--- src/main/java/com/example/sbstudy/controller/UserController.java
+++ src/main/java/com/example/sbstudy/controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.sbstudy.controller;
 
 
 import com.example.sbstudy.entity.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -20,6 +23,8 @@ public class UserController {
         return r;
     }
 
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     @RequestMapping(value="/", method=RequestMethod.POST)
     public String postUser(@ModelAttribute User user) {
         // 处理"/users/"的POST请求，用来创建User
@@ -35,6 +40,11 @@ public class UserController {
         return users.get(id);
     }
 
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+  /*  @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })*/
     @RequestMapping(value="/{id}", method=RequestMethod.PUT)
     public String putUser(@PathVariable Long id, @ModelAttribute User user) {
         // 处理"/users/{id}"的PUT请求，用来更新User信息
